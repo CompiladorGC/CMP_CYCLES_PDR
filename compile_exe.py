fb009b03c0849593416df32dea2dc50afd13ab46
Takauã Silva <takauan9@gmail.com>
2026-01-01 19:15:00 -0300

    Implementado classe responsável por gerar exe dos ciclos

--- compile_exe.py
+++ compile_exe.py
@@ -1,5 +1,9 @@
 from src.controller.gerar_exe import GerarExe
 
 if __name__ == '__main__':
-    exe = GerarExe()
+    exe = GerarExe(
+        script_path='src/model/cycles/cycle_desbaste.py',
+        output_path='dist/cycle_desbaste/v1.0',
+        output_name='ciclo_desbaste')   
     exe.compile_exe()
+    exe.clean_files()
--- src/controller/gerar_exe.py
+++ src/controller/gerar_exe.py
@@ -1,11 +1,12 @@
-import os, subprocess
+import sys, os, subprocess, shutil
 from src.controller.cmds_terminal import CmdsTerminal
 
-# Classe responsável por gerar scripts de .py para .exe
+# Classe responsável por gerar exe dos ciclos do compilador g-code
 class GerarExe:
-    def __init__(self, script_path: str, output_name: str, one_file: bool=None, windowed: bool=None, icon_path: str=None):
-        
+    def __init__(self, script_path: str, output_path: str, output_name: str, one_file=True, windowed=True, icon_path='assets/favicon.ico'):
+
         self.script_path = script_path
+        self.output_path = output_path
         self.output_name = output_name
         self.icon_path = icon_path
         self.one_file = one_file
@@ -14,17 +15,28 @@ class GerarExe:
         self.cmd = CmdsTerminal()
         self.validate()
 
-    # Método responsável por validar o caminho do script
+    # Método responsável por validadr os campos de parâmetros da classe
     def validate(self):
         if not os.path.exists(self.script_path):
             raise FileNotFoundError(f'[ERRO] O script não foi encontrado: {self.script_path}')
-        
-        if self.icon_path and not os.path.exists(self.icon_path):
-            raise FileNotFoundError(f'[ERRO] O icone não foi encotrado: {self.icon_path}')
 
-    # Método responsável por compilar o script py
+        if self.icon_path and not os.path.exists(self.icon_path):
+            raise FileNotFoundError(f'[ERRO] O icone não foi encontrado: {self.icon_path}')
+
+    # Método responsável por remover arquivos e pastas que são geradas com .exe
+    def clean_files(self):
+        # Remove o arquivo .spec
+        spec_file = f'{self.output_name}.spec'
+        if os.path.exists(spec_file):
+            os.remove(spec_file)
+
+        # Remove a pasta build
+        if os.path.exists('build'):
+            shutil.rmtree('build')
+
+    # Método responsável executar os comandos pyinstaller para gerar o .exe
     def compile_exe(self):
-        command = ['pyinstaller', self.script_path, "--name", self.output_name]
+        command = [sys.executable, "-m", "PyInstaller", self.script_path, "--name", self.output_name]
 
         if self.one_file:
             command.append('--onefile')
@@ -37,12 +49,15 @@ class GerarExe:
 
         self.cmd.msg('\nGERANDO EXECUTÁVEL...', 4)
 
-        result = subprocess.run(command, 
-                                stdout=subprocess.PIPE,
-                                stderr=subprocess.PIPE, 
-                                text=True)
+        result = subprocess.run(
+            command,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True
+        )
 
         if result.returncode != 0:
+            print(result.stderr)
             raise RuntimeError('[ERRO] Falha em gerar o executável.')
-        
-        self.cmd.msg('\nEXECUTÁVEL GERADO SUCESSO!', 5)
+
+        self.cmd.msg('\nEXECUTÁVEL GERADO COM SUCESSO!', 5)
